002f97e19b3740233c7bbf51b6aeb1c666521e63
zhuodannychen <zhuodannychen@gmail.com>
2019-10-13 19:30:25 -0500

    edit home screen

--- screens/start_screen.py
+++ screens/start_screen.py
@@ -1,3 +1,4 @@
+import pygame
 from screens import util
 
 
@@ -5,17 +6,29 @@ def main(screen, clock):
     bg = util.get_image('background')
     bg_h = 934
     logo = util.get_image('logo')
+    start = util.get_image('start_button')
+    start = pygame.transform.scale(start, (350, 150))
+    option = util.get_image('options_button')
+    option = pygame.transform.scale(option, (400, 150))
 
     bg_animation_ticks = 200
     bg_ease = util.EaseOutSine(bg_animation_ticks, util.HEIGHT - bg_h, 0)
     logo_ease = util.EaseOutSine(bg_animation_ticks, -50, -160)
-
+    
     for i in range(bg_animation_ticks):
         # Initialize & scroll start screen
         screen.fill([0, 255, 0])
         screen.blit(bg, (0, next(bg_ease)))
         screen.blit(logo, (0, next(logo_ease)))
 
+        if not util.tick(clock):
+            return False
+
+    for i in range(0, 255):
+        start.set_alpha(i/20) # doesn't make the image fully opaque
+        option.set_alpha(i/20)
+        screen.blit(start, (450, 350))
+        screen.blit(option, (420, 500))
         if not util.tick(clock):
             return False
 
